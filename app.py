678ae6879e0a2773f2285204122eeb3f3cde1c11
DCZare <88459946+DCZare@users.noreply.github.com>
2024-09-18 17:00:15 -0400

    route structure

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '''
-        <html>
-            <head><title>ORCID Auth</title></head>
-            <body>
-                <h1>Welcome to the ORCID Auth Example</h1>
-                <a href="/redirect">Redirect to Welcome Page</a>
-            </body>
-        </html>
-    '''
+    return render_template('index.html')  # Serve index.html
 
 @app.route('/redirect')
 def redirect_to_welcome():
     return redirect('https://dczare.github.io/UBNS_Bibliometrics/welcome.html')
 
 @app.route('/home')
-def redirect_to_home():
-    return redirect('https://dczare.github.io/UBNS_Bibliometrics/home.html')
+def home():
+    return render_template('home.html')  # Serve home.html
 
 if __name__ == '__main__':
     app.run(debug=True)
