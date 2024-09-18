e95384a47c934447512c26bebbe3537923f715f8
DCZare <88459946+DCZare@users.noreply.github.com>
2024-09-18 17:54:42 -0400

    Elementary Dynamic Routing

--- app.py
+++ app.py
@@ -1,44 +1,21 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '''
-        <html>
-            <head><title>ORCID Auth</title></head>
-            <body>
-                <h1>Welcome to the ORCID Auth Example</h1>
-                <a href="/redirect">Redirect to Welcome Page</a>
-                <br>
-                <a href="/search">Search for Something</a>
-                <br><br>
-                <form action="/search" method="get">
-                    <input type="text" name="query" placeholder="Enter search term">
-                    <button type="submit">Search</button>
-                </form>
-            </body>
-        </html>
-    '''
-
-@app.route('/redirect')
-def redirect_to_welcome():
-    if 'localhost' in request.host:
-        return redirect(url_for('local_welcome'))
-    else:
-        return redirect('https://dczare.github.io/UBNS_Bibliometrics/welcome.html')
-
-@app.route('/welcome')
-def local_welcome():
-    return render_template('welcome.html')
+    # Redirecting to the home route
+    return redirect(url_for('home'))
 
 @app.route('/home')
 def home():
+    # Rendering the home page
     return render_template('home.html')
 
-@app.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query', '')  # Retrieves the 'query' parameter
+@app.route('/search_results')
+def search_results():
+    query = request.args.get('query', '')
+    # Rendering the search results page with the query parameter
     return render_template('search_results.html', query=query)
 
 if __name__ == '__main__':
