fe52308ec538018805369bc07d3ad83130209444
DCZare <88459946+DCZare@users.noreply.github.com>
2024-09-18 17:27:07 -0400

    Search configuring

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -12,19 +12,31 @@ def index():
                 <a href="/redirect">Redirect to Welcome Page</a>
                 <br>
                 <a href="/search">Search for Something</a>
+                <br><br>
+                <form action="/search" method="get">
+                    <input type="text" name="query" placeholder="Enter search term">
+                    <button type="submit">Search</button>
+                </form>
             </body>
         </html>
     '''
 
 @app.route('/redirect')
 def redirect_to_welcome():
-    return redirect('https://dczare.github.io/UBNS_Bibliometrics/welcome.html')
+    if 'localhost' in request.host:
+        return redirect(url_for('local_welcome'))
+    else:
+        return redirect('https://dczare.github.io/UBNS_Bibliometrics/welcome.html')
+
+@app.route('/welcome')
+def local_welcome():
+    return render_template('welcome.html')
 
 @app.route('/home')
 def home():
-    return render_template('home.html')  # Renders the home.html template
+    return render_template('home.html')
 
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query', '')  # Retrieves the 'query' parameter
     return render_template('search_results.html', query=query)
