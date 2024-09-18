03106b467ef3925dbc2b5ab9654615515516af74
DCZare <88459946+DCZare@users.noreply.github.com>
2024-09-18 12:50:08 -0400

    Minor adjustments

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return '''
+        <html>
+            <head><title>ORCID Auth</title></head>
+            <body>
+                <h1>Welcome to the ORCID Auth Example</h1>
+                <a href="/redirect">Redirect to Welcome Page</a>
+            </body>
+        </html>
+    '''
 
-@app.route('/welcome')
-def welcome():
-    return '<h1>Welcome! You have successfully authenticated with ORCID.</h1>'
+@app.route('/redirect')
+def redirect_to_welcome():
+    return redirect('https://dczare.github.io/UBNS_Bibliometrics/welcome.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
