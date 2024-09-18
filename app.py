3f54ab8e93ed2941502b1dfba5777c1720541f7f
DCZare <88459946+DCZare@users.noreply.github.com>
2024-09-18 18:05:40 -0400

    cleanup

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+# Default landing page (Welcome Page)
 @app.route('/')
-def index():
-    # Redirecting to the home route
-    return redirect(url_for('home'))
+def welcome():
+    return render_template('welcome.html')
 
+# Home Page (where the user is redirected after clicking login)
 @app.route('/home')
 def home():
-    # Rendering the home page
     return render_template('home.html')
 
-@app.route('/search_results')
-def search_results():
+# Search Route with dynamic results
+@app.route('/search', methods=['GET'])
+def search():
     query = request.args.get('query', '')
-    # Rendering the search results page with the query parameter
+    return redirect(url_for('dynamic_search_results', query=query))
+
+# Dynamic search results page
+@app.route('/search/results/<query>')
+def dynamic_search_results(query):
     return render_template('search_results.html', query=query)
 
 if __name__ == '__main__':
